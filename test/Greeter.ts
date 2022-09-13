3fb4b8ef456907f40e78ed57cc90df61b0e21d2d
JulissaDantes <julissadcj@gmail.com>
2022-09-13 14:26:56 -0400

    add test

--- test/Greeter.ts
+++ test/Greeter.ts
@@ -1,4 +1,4 @@
-import { AccountId, Wallet } from '@hashgraph/sdk';
+import { Client, ContractCallQuery, ContractFunctionParameters, Wallet, Hbar, ContractId } from '@hashgraph/sdk';
 import { expect } from 'chai';
 import * as fs from "fs";
 import dotenv from "dotenv";
@@ -7,25 +7,56 @@ import { Greeter } from '../typechain-types';
 
 dotenv.config()
 
+const operatorId = process.env.OPERATOR_ACCOUNT_ID!;
+const operatorPrivateKey = process.env.OPERATOR_PRIVATE_KEY!;
+const client = Client.forTestnet().setOperator(operatorId, operatorPrivateKey).setDefaultMaxTransactionFee(new Hbar(10));
+
 describe('RPC', function() {
   const init_message = 'initial_msg';
   let contractAddress: any;
   let wallet: Wallet;
   let provider: Provider;
   let greeter: Greeter;
+  let contractID: ContractId;
+
   before(async () => {
     provider = await new ethers.providers.JsonRpcProvider(process.env.RELAY_ENDPOINT);
     wallet = await new ethers.Wallet( process.env.OPERATOR_PRIVATE_KEY, provider);
     const Greeter = await ethers.getContractFactory('Greeter', wallet);
     greeter = await Greeter.deploy(init_message);
-    contractAddress = (await greeter.deployTransaction.wait()).contractAddress;
+    const receipt = await greeter.deployTransaction.wait();
+    contractID = ContractId.fromSolidityAddress(receipt.contractAddress);
+    console.log(contractID);
   });
+
   it('should be able to deploy a contract', async function() {
     expect(contractAddress).to.not.be.null;
   });
+
   it('should be able to call a contract', async function() {
-    const result = await greeter.greet();
-    console.log(result);
-    expect(result).to.be.equal(init_message);
+    const partialTxParams = await greeter.populateTransaction.greet() as any;
+
+    const fullTxParams = await wallet.populateTransaction(partialTxParams);
+    const signedTx = await wallet.signTransaction(fullTxParams);
+
+    const tx = await ethers.provider.send('eth_sendRawTransaction', [signedTx]);
+    expect(tx).to.not.be.null;
+  });
+
+  it('should be able to call a contract and read a returned value', async function() {
+    const contractQueryTx1 = new ContractCallQuery()
+    .setContractId(contractID)
+    .setGas(100000)
+    .setFunction(
+      "greet",
+      new ContractFunctionParameters()
+    );
+
+    const contractQuerySubmit1 = await contractQueryTx1.execute(client);
+    const contractQueryResult1 = contractQuerySubmit1.getUint256(0);
+    console.log(
+      `- Here's the greeting you asked for: ${contractQueryResult1} \n`
+    );
+    expect(contractQueryResult1).to.be.equal(init_message);
   });
 });
